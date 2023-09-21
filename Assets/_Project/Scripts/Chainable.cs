a121664a9291b2ed0250df0c2ffe701c040b6c66
boxcatdev <boxcatdev@gmail.com>
2023-09-21 17:32:03 -0500

    chain damage working
    - also added proxy images for rocks and trees

--- Assets/_Project/Scripts/Chainable.cs
+++ Assets/_Project/Scripts/Chainable.cs
@@ -1,21 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Chainable : MonoBehaviour
 {
+    private MeshRenderer meshRenderer;
+
+    [SerializeField] Material defaultMat;
+    [SerializeField] Material hitMat;
+
+    [SerializeField] float hitRange = 3f;
+
+    public bool canTriggerHits = false;
+    public bool hasBeenHit = false;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponentInChildren<MeshRenderer>();
+    }
     private void Start()
     {
-        //Debug.Log(name);
+        if(!hasBeenHit)
+            if(defaultMat != null) meshRenderer.material = defaultMat;
+        else
+            if (hitMat != null) meshRenderer.material = hitMat;
     }
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("text");
 
-        Chainable chainable = other.GetComponent<Chainable>();
-        if (chainable != null)
+    private void Update()
+    {
+        if (canTriggerHits)
         {
-            Debug.Log(chainable.name);
+            if (Keyboard.current.fKey.wasPressedThisFrame)
+            {
+                DoHitCheck();
+            }
         }
+    }
+
+    public void DoHitCheck()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, hitRange);
+        foreach (var collider in colliders)
+        {
+            if(collider.TryGetComponent(out Chainable chainable))
+            {
+                if (chainable != this && chainable.hasBeenHit == false)
+                {
+                    Debug.Log(name + " found: " + chainable.name);
+
+                    chainable.GetHit();
+
+                }
+            }
+        }
+    }
+
+    public void GetHit()
+    {
+        hasBeenHit = true;
+
+        if (hitMat != null) meshRenderer.material = hitMat;
+
+        DoHitCheck();
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        if(!hasBeenHit)
+            Gizmos.DrawWireSphere(transform.position, hitRange);
     }
 }
