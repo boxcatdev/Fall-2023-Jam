b335927d19dcccb3b0735b7185cc73a00c021149
Nick Hanson <nickalex725@gmail.com>
2023-09-24 01:48:11 -0500

    added health script and basic enemy AI

--- Assets/_Project/Scripts/Enemy.cs
+++ Assets/_Project/Scripts/Enemy.cs
@@ -1,20 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    private Rigidbody _rb;
+    [SerializeField] private int _damage;
+    [SerializeField] private float _damageCooldown;
+    [SerializeField] private int _attackRange;
 
+    private GameObject _targetPlayer;
+    private Health _playerHealth;
+    private Rigidbody _rb;
+    private NavMeshAgent _navMeshAgent;
+    private float _currentDamageCooldown;
+    private bool _onCooldown = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Enemy>())
+        {
+            _rb.Sleep();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(!_onCooldown && other.GetComponent<Lasso>())
+        {
+            _playerHealth.TakeDamage(_damage);
+            _onCooldown = true;
+        }
+    }
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _targetPlayer = GameObject.FindGameObjectWithTag("Player");
+        _playerHealth = _targetPlayer.GetComponent<Health>();
+        _currentDamageCooldown = _damageCooldown;
     }
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        if(other.GetComponent<Enemy>())
+        if(_targetPlayer != null)
         {
-           _rb.Sleep();
+            _navMeshAgent.destination = _targetPlayer.transform.position;
+        }
+        if (_onCooldown)
+        {
+            _currentDamageCooldown -= Time.deltaTime;
+            if (_currentDamageCooldown <= 0)
+            {
+                _onCooldown = false;
+                _currentDamageCooldown = _damageCooldown;
+            }
         }
     }
 }
