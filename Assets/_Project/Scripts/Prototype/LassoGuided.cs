37b0281724c6e47db0dcc75bdfabd832cb936206
boxcatdev <boxcatdev@gmail.com>
2023-09-26 01:33:41 -0500

    cleaned up code a bit
    - going to modify to keep visuals on the ground plane

--- Assets/_Project/Scripts/Prototype/LassoGuided.cs
+++ Assets/_Project/Scripts/Prototype/LassoGuided.cs
@@ -8,14 +8,13 @@ public class LassoGuided : MonoBehaviour
     [SerializeField] Transform _mouseTarget;
     [SerializeField] Transform _lassoVisual;
     [Space]
+    public int pullForce;
     public float lassoSize = 0.0f;
     public float maxSize = 5.0f;
     public float growRate = 0.01f;
-    [Space]
+    [Space] //hide these later, only used for testing
     public bool isMovingLasso = false;
     public bool isChangingSize = false;
-
-    private int count = 0;
 
     private StarterAssets.StarterAssetsInputs inputs;
 
@@ -33,36 +32,54 @@ public class LassoGuided : MonoBehaviour
         //inputs
         if (Mouse.current.rightButton.wasPressedThisFrame)
         {
-            if (isChangingSize) return;
-
-            //switch lasso bool
-            isMovingLasso = !isMovingLasso;
-
-            RefreshVisuals();
+            //ToggleLasso();
+            StartLasso();
+        }
+        if (Mouse.current.rightButton.wasReleasedThisFrame)
+        {
+            StopLasso();
         }
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            if (!isMovingLasso) return;
-
-            //switch changing bool
-            isChangingSize = true;
-
-            //start changing lasso size
-            lassoSize = 0.01f;
-
-            RefreshVisuals();
+            StartGrow();
         }
         if (Mouse.current.leftButton.wasReleasedThisFrame)
         {
-            if (!isMovingLasso) return;
-
-            //switch changing bool
-            isChangingSize = false;
-            isMovingLasso = false;
-
-            RefreshVisuals();
+            StopGrow();
         }
 
+        //change cursor state
+        UpdateCursorState();
+
+        //enable/disable visual
+        //RefreshVisuals();
+
+        //move target to mouse position
+        if (_lassoVisual != null && _mouseTarget != null)
+        {
+            if (_lassoVisual.gameObject.activeInHierarchy) _lassoVisual.position = Utility.GetMouseHitPoint();
+            if (_mouseTarget.gameObject.activeInHierarchy) _mouseTarget.position = Utility.GetMouseHitPoint();
+        }
+        #endregion
+
+        #region Scale lasso
+        if (isChangingSize)
+        {
+            Debug.Log("Resizing");
+            lassoSize += growRate * Time.deltaTime;
+
+            if(lassoSize >= maxSize)
+            {
+                lassoSize = maxSize;
+            }
+
+            ScaleLasso(lassoSize);
+        }
+        #endregion
+    }
+    #region Helpers
+    private void UpdateCursorState()
+    {
         //change cursor state
         if (isMovingLasso)
         {
@@ -74,40 +91,15 @@ public class LassoGuided : MonoBehaviour
             if (Cursor.lockState == CursorLockMode.Confined) Cursor.lockState = CursorLockMode.Locked;
             if (inputs.cursorInputForLook == false) inputs.cursorInputForLook = true;
         }
-
-        //enable/disable visual
-        //RefreshVisuals();
-
-        //move target to mouse position
-        if (_lassoVisual != null && _mouseTarget != null)
-        {
-            if (isMovingLasso)
-            {
-                _lassoVisual.position = Utility.GetMouseHitPoint();
-                _mouseTarget.position = Utility.GetMouseHitPoint();
-            }
-        }
-        #endregion
-
-        #region Lasso Size
-        if (isChangingSize)
-        {
-            Debug.LogWarning("Resizing");
-            lassoSize += growRate * Time.deltaTime;
-
-            if(lassoSize >= maxSize)
-            {
-
-            }
-
-            ScaleLasso(lassoSize);
-        }
-        #endregion
     }
+    /// <summary>
+    /// Activate/deactivate visuals based on bools.
+    /// </summary>
     private void RefreshVisuals()
     {
-        count++;
-        Debug.LogFormat("RefreshVisuals() [{0}]", count);
+        if (_mouseTarget == null || _lassoVisual == null) return;
+
+        // activate/deactivate visuals based on bools
         if (isMovingLasso == true && isChangingSize == false)
         {
             //show cursor hide lasso
@@ -127,23 +119,93 @@ public class LassoGuided : MonoBehaviour
             _lassoVisual.gameObject.SetActive(false);
         }
     }
-    private void ToggleLasso()
-    {
-
-    }
-    private void StartGrow()
-    {
-
-    }
-    private void EndGrow()
-    {
-
-    }
+    /// <summary>
+    /// Resize the visual for lasso range (to be changed during polish).
+    /// </summary>
+    /// <param name="scale"></param>
     private void ScaleLasso(float scale)
     {
         Transform child = _lassoVisual.GetChild(0);
 
+        if (child.localScale.x == scale) return;
+
         Vector3 adjustedScale = new Vector3(scale, child.localScale.y, scale);
         child.localScale = adjustedScale;
     }
+    #endregion
+
+    #region Lasso State
+    private void ToggleLasso()
+    {
+        if (isChangingSize) return;
+
+        //switch lasso bool
+        isMovingLasso = !isMovingLasso;
+
+        RefreshVisuals();
+    }
+    private void StartLasso()
+    {
+        if (isChangingSize) return;
+
+        //switch lasso bool
+        isMovingLasso = true;
+
+        RefreshVisuals();
+    }
+    private void StopLasso()
+    {
+        //do action if ready
+        if (isChangingSize) DoLassoAction();
+
+        //switch lasso bool
+        isMovingLasso = false;
+        isChangingSize = false;
+
+        RefreshVisuals();
+    }
+    private void StartGrow()
+    {
+        if (!isMovingLasso) return;
+
+        //switch changing bool
+        isChangingSize = true;
+
+        //start changing lasso size
+        lassoSize = 0.01f;
+
+        RefreshVisuals();
+    }
+    private void StopGrow()
+    {
+        //do action if ready
+        if(isMovingLasso) DoLassoAction();
+
+        if (!isMovingLasso) return;
+
+        //switch changing bool
+        isChangingSize = false;
+        isMovingLasso = false;
+
+        RefreshVisuals();
+    }
+    #endregion
+
+    #region Lasso Action
+    private void DoLassoAction()
+    {
+        Debug.LogWarning("DoLassoAction()");
+
+        //copied from LassoProjectile but substituting variables
+        Collider[] _enemiesInRange = Physics.OverlapSphere(transform.position, lassoSize);
+        for (int i = 0; i < _enemiesInRange.Length; i++)
+        {
+            if (_enemiesInRange[i].gameObject.TryGetComponent(out Chainable chainable))
+            {
+                _enemiesInRange[i].transform.position = Vector3.MoveTowards(_enemiesInRange[i].transform.position, 
+                    Utility.GetMouseHitPoint(), pullForce * Time.deltaTime);
+            }
+        }
+    }
+    #endregion
 }
