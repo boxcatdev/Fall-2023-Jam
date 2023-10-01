f55b6c2c4e5209bcbc3b80255d0f9f5ff2af6187
Nick Hanson <nickalex725@gmail.com>
2023-09-30 23:12:28 -0500

    health

--- Assets/_Project/Scripts/Game/Health.cs
+++ Assets/_Project/Scripts/Game/Health.cs
@@ -12,12 +12,13 @@ public class Health : MonoBehaviour
     [SerializeField] private ParticleSystem _deathEffect;
     [SerializeField] private AudioSource _hitSound;
 
+    [SerializeField] private List<Image> _healthBats;
+
 
     // Start is called before the first frame update
     void Start()
     {
         _currentHealth = _maxHealth;
-
         RefreshUI();
     }
 
@@ -27,6 +28,7 @@ public class Health : MonoBehaviour
         if (_hitSound != null) _hitSound.Play();
         if (_currentHealth <= 0)
         {
+            RefreshUI();
             Destroy(gameObject);
             if(_deathEffect != null) Instantiate(_deathEffect, transform.position, Quaternion.identity);
         }
@@ -48,11 +50,16 @@ public class Health : MonoBehaviour
 
     private void RefreshUI()
     {
-        if (_healthSlider)
+        for (int i = 0; i < _healthBats.Count; i++)
         {
-            if (_healthSlider.maxValue != _maxHealth) _healthSlider.maxValue = _maxHealth;
-
-            if (_healthSlider.value != _currentHealth) _healthSlider.value = _currentHealth;
+            if(i < _currentHealth)
+            {
+                _healthBats[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                _healthBats[i].gameObject.SetActive(false);
+            }
         }
     }
 }
